7258a0c065ca64cde52aad8289104ce939a7343c
冰紅茶 <icealtria@pm.me>
2022-08-20 01:47:28 +0800

    categorize

---
+++ Homeworks/hw06/hw06.scm
@@ -0,0 +1,33 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
+
+(define (interleave lst1 lst2) 
+    (cond 
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))
+    )
+)
+
+(define (my-filter pred lst) 
+    (cond 
+        ((null? lst) nil)
+        ((pred (car lst)) 
+            (cons (car lst) (my-filter pred (cdr lst)))
+            )
+        (else (my-filter pred (cdr lst)))
+        )
+)
+
+(define (concatenate s) 
+    (define (helper concatenated rest)
+        (if (null? rest)
+            concatenated
+            (helper (append concatenated (car rest)) (cdr rest))
+        )
+    )
+(helper () s)
+)
