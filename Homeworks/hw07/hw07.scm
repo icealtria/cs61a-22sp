bff5953ed9032f441245ced8ddf2df5a0f0b559e
冰紅茶 <icealtria@pm.me>
2022-08-28 00:45:29 +0800

    hw07

---
+++ Homeworks/hw07/hw07.scm
@@ -0,0 +1,39 @@
+(define (cadr lst) (car (cdr lst)))
+
+(define (make-kwlist1 keys values)
+    (list keys values))
+
+(define (get-keys-kwlist1 kwlist)
+    (car kwlist))
+
+(define (get-values-kwlist1 kwlist)
+    (cadr kwlist))
+
+(define (make-kwlist2 keys values)
+    (if (null? keys)
+        nil
+        (cons (list (car keys) (car values)) (make-kwlist2 (cdr keys) (cdr values)))))
+
+(define (get-keys-kwlist2 kwlist) 
+    (if (null? kwlist)
+        nil
+        (cons (car (car kwlist)) (get-keys-kwlist2 (cdr kwlist)))))
+
+(define (get-values-kwlist2 kwlist)
+    (if (null? kwlist)
+        nil
+        (cons (cadr (car kwlist)) (get-values-kwlist2 (cdr kwlist)))))
+
+(define (add-to-kwlist kwlist key value)
+    (make-kwlist 
+                 (append (get-keys-kwlist kwlist) (list key))
+                 (append (get-values-kwlist kwlist) (list value))))
+
+(define (get-first-from-kwlist kwlist key)
+    (define keys (get-keys-kwlist kwlist))
+    (define vals (get-values-kwlist kwlist))
+    (cond
+        ((null? keys) nil)
+        ((equal? (car keys) key) (car vals))
+        (else (get-first-from-kwlist (make-kwlist (cdr keys) (cdr vals)) key)))
+)
